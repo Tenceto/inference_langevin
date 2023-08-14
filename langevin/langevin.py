7a4ea19b67b63157ccb72a8629dccab4d719d25b
Tenceto <martin_s8@hotmail.com>
2023-08-14 14:50:32 -0500

    Learn theta as well

--- langevin/langevin.py
+++ langevin/langevin.py
@@ -3,153 +3,105 @@ from langevin.utils import compute_aucroc, compute_relative_error
 
 
 class LangevinEstimator:
-    def __init__(self, h_theta, A_score_model, sigma_e, theta_prior_dist=None, theta_fixed=None):
+    def __init__(self, h_theta, A_score_model, sigma_e, theta_prior_dist):
         self.h_theta = h_theta
         self.A_score_model = A_score_model
         self.sigma_e = sigma_e
-
-        if theta_fixed is not None:
-            self.theta_tilde = theta_fixed
-            self.estimate_theta = False
-        else:
-            assert type(theta_prior_dist) is torch.distributions.normal.Normal, "Only normal prior is supported for theta."
-            self.theta_prior_mean = theta_prior_dist.loc
-            self.theta_prior_var = theta_prior_dist.scale ** 2
-            self.estimate_theta = True
-            # This distribution will be updated every time the noise level changes
-            self.theta_prior_dist = None
-        
+        self.theta_prior_dist = theta_prior_dist
         self.metrics = None
 
-    def score_joint_likelihood(self, X, Y, grad_variable):
-        if grad_variable == "A":
-            A = self.A_tilde.clone().requires_grad_(True)
-            # We only need the upper triangular part of A to account
-            # for the symmetry of the matrix, so that the gradient is correct
-            F = self.h_theta(torch.triu(A) + torch.triu(A, diagonal=1).T, self.theta_tilde)
-        elif grad_variable == "theta":
-            theta = self.theta_tilde.clone().requires_grad_(True)
-            F = self.h_theta(self.A_tilde, theta)
+    def score_graph_likelihood(self, A, X, Y, theta):
+        A = A.clone().requires_grad_(True)
+        # We only need the upper triangular part of A to account
+        # for the symmetry of the matrix, so that the gradient is correct
+        log_likelihood = - self.compute_minus_likelihood(torch.triu(A) + torch.triu(A, diagonal=1).T, 
+                                                         X, Y, theta)
+        grad = torch.autograd.grad(log_likelihood, A)[0]
+        return grad
 
+    def compute_minus_likelihood(self, A, X, Y, theta):
+        F = self.h_theta(A, theta)
         log_likelihood = - 1 / (2 * self.sigma_e ** 2) * (torch.linalg.norm(Y - F @ X, dim=0) ** 2).sum()
-        
-        if grad_variable == "A":
-            grad = torch.autograd.grad(log_likelihood, A)[0]
-        elif grad_variable == "theta":
-            grad = torch.autograd.grad(log_likelihood, theta)[0]
-            
-        return grad
-    
-    def compute_theta_prior_score(self):
-        theta = self.theta_tilde.clone().requires_grad_(True)
-        log_prob = self.theta_prior_dist.log_prob(theta)
-        grad = torch.autograd.grad(log_prob, theta)[0]
-
-        return grad
+        return - log_likelihood
 
     def langevin_estimate(self, A_nan, X, Y, 
-                          sigmas_sq_A, sigmas_sq_theta, 
-                          epsilon_A, epsilon_theta, 
-                          temperature_A, temperature_theta,
-                          steps,
+                          sigmas_sq, epsilon, temperature, steps, adam_lr,
                           projection_method="rounding", clip_A_tilde=False, true_A=None, true_theta=None):
-
-        assert len(sigmas_sq_A) == len(sigmas_sq_theta), "sigmas_sq_A and sigmas_sq_theta must have the same length."
-
-        # Initialize theta_tilde if it isn't fixed
-        if self.estimate_theta:
-            self.update_theta_annealed_prior(sigmas_sq_theta[0])
-            self.theta_tilde = self.theta_prior_dist.sample()
+        
+        # Initialize theta_tilde
+        theta_tilde = self.theta_prior_dist.sample().abs()
+        # We define this tensor to be used for the optimizer without screwing up the gradients
+        theta_grad = theta_tilde.requires_grad_(True)
+        optimizer = torch.optim.Adam([theta_grad], lr=adam_lr)
 
         unknown_idxs = torch.where(torch.isnan(torch.triu(A_nan)))
         known_mask = ~ torch.isnan(A_nan)
         size_unknown = len(unknown_idxs[0])
-        
-        dim_theta = len([self.theta_tilde])
-
-        # Distributions for the Langevin noise (NOT the annealing one, these have unitary variance)
+        # Distribution for the Langevin noise (NOT the annealing one, this one has unitary variance)
         z_dist = torch.distributions.MultivariateNormal(torch.zeros(size_unknown), torch.eye(size_unknown))
-        v_dist = torch.distributions.MultivariateNormal(torch.zeros(dim_theta), torch.eye(dim_theta))
 
         # Initialize A_tilde and its projection
-        self.A_tilde = torch.distributions.Normal(0.5, 0.1).sample(A_nan.shape)
-        self.A_tilde = 0.5 * (torch.triu(self.A_tilde) + torch.triu(self.A_tilde, 1).T)
-        self.A_tilde.fill_diagonal_(0.0)
-        self.A_tilde[known_mask] = A_nan[known_mask]
-        self.A_proj = self.project_adjacency_matrix(projection_method)
+        A_tilde = torch.distributions.Normal(0.5, 0.1).sample(A_nan.shape)
+        A_tilde = 0.5 * (torch.triu(A_tilde) + torch.triu(A_tilde, 1).T)
+        A_tilde.fill_diagonal_(0.0)
+        A_tilde[known_mask] = A_nan[known_mask]
+        A_proj = self.project_adjacency_matrix(A_tilde, projection_method)
 
-        if true_A is not None:
+        if true_A is not None and true_theta is not None:
             compute_metrics = True
-            self.metrics = {"aucroc": torch.empty(steps * len(sigmas_sq_A))}
-        else:
-            compute_metrics = False
-        if self.estimate_theta and true_theta is not None:
-            self.metrics["relative_error"] = torch.empty(steps * len(sigmas_sq_theta))
+            metrics = {"aucroc": torch.empty(steps * len(sigmas_sq)),
+                       "relative_error": torch.empty(steps * len(sigmas_sq))}
 
-        for sigma_i_idx, (sigma_i_sq_A, sigma_i_sq_theta) in enumerate(zip(sigmas_sq_A, sigmas_sq_theta)):
-            alpha_A = epsilon_A * sigma_i_sq_A / sigmas_sq_A[-1]
-            alpha_theta = epsilon_theta * sigma_i_sq_theta / sigmas_sq_theta[-1]
-            # sigma_i = torch.sqrt(sigma_i_sq)
+        for sigma_i_idx, sigma_i_sq in enumerate(sigmas_sq):
+            alpha = epsilon * sigma_i_sq / sigmas_sq[-1]
             for t in range(steps):
-                if torch.any(torch.isnan(self.A_tilde)):
-                    print("A_tilde contains NaNs")
-                    break
                 z = z_dist.sample([1])
-                v = v_dist.sample([1])
-                
+                # Compute the score
+                score_prior_A = self.A_score_model(A_tilde, sigma_i_idx)[unknown_idxs[0], unknown_idxs[1]]
+                score_likelihood_A = self.score_graph_likelihood(A_tilde, X, Y, theta_tilde)[unknown_idxs[0], unknown_idxs[1]]
                 # Update A
-                score_prior_A = self.A_score_model(self.A_tilde, sigma_i_idx)[unknown_idxs[0], unknown_idxs[1]]
-                score_likelihood_A = self.score_joint_likelihood(X, Y, grad_variable="A")[unknown_idxs[0], unknown_idxs[1]]
-                
-                self.A_tilde[unknown_idxs[0], unknown_idxs[1]] = (self.A_tilde[unknown_idxs[0], unknown_idxs[1]]
-                                                                  + alpha_A * (score_prior_A + score_likelihood_A)
-                                                                  + torch.sqrt(2 * alpha_A * temperature_A) * z)
-                self.A_tilde[unknown_idxs[1], unknown_idxs[0]] = self.A_tilde[unknown_idxs[0], unknown_idxs[1]]
-                
+                A_tilde[unknown_idxs[0], unknown_idxs[1]] = (A_tilde[unknown_idxs[0], unknown_idxs[1]]
+                                                             + alpha * (score_prior_A + score_likelihood_A)
+                                                             + torch.sqrt(2 * alpha * temperature) * z)
+                A_tilde[unknown_idxs[1], unknown_idxs[0]] = A_tilde[unknown_idxs[0], unknown_idxs[1]]
                 if clip_A_tilde:
-                    self.A_tilde = self.clip_adjacency_matrix(torch.sqrt(sigma_i_sq_A))
-
-                self.A_proj = self.project_adjacency_matrix(projection_method)
+                    A_tilde = self.clip_adjacency_matrix(A_tilde, torch.sqrt(sigma_i_sq))
+                A_proj = self.project_adjacency_matrix(A_tilde, projection_method)
 
                 # Update theta
-                if self.estimate_theta:
-                    score_prior_theta = self.compute_theta_prior_score()
-                    score_likelihood_theta = self.score_joint_likelihood(X, Y, grad_variable="theta")
-                    self.theta_tilde = (self.theta_tilde 
-                                        + alpha_theta * (score_prior_theta + score_likelihood_theta) 
-                                        + torch.sqrt(2 * alpha_theta * temperature_theta) * v)
-                    # TODO: This is only for the exponential filter, because if theta is negative the filter will diverge
-                    self.theta_tilde = torch.clip(self.theta_tilde, min=0.0)
+                optimizer.zero_grad()
+                loss = self.compute_minus_likelihood(A_proj, X, Y, theta_grad)
+                loss.backward()
+                optimizer.step()
+                # TODO: This is only for the exponential filter, because if theta is negative the filter will diverge
+                with torch.no_grad():
+                    theta_tilde = torch.clip(theta_grad.detach().clone(), min=0.0)
+                    # Compute metrics
+                    if compute_metrics:
+                        n_step = steps * sigma_i_idx + t
+                        metrics["aucroc"][n_step] = compute_aucroc(true_A, A_tilde, use_idxs=unknown_idxs)
+                        metrics["relative_error"][n_step] = compute_relative_error(true_theta, theta_tilde)
 
-                # Compute metrics
-                if compute_metrics:
-                    self.metrics["aucroc"][steps * sigma_i_idx + t] = compute_aucroc(true_A, self.A_tilde, use_idxs=unknown_idxs)
-                    if self.estimate_theta and true_theta is not None:
-                        self.metrics["relative_error"][steps * sigma_i_idx + t] = compute_relative_error(true_theta, self.theta_tilde)
-
-        return None
+        return A_tilde, theta_tilde, metrics
     
-    def project_adjacency_matrix(self, projection_method):
+    def project_adjacency_matrix(self, A_tilde, projection_method):
         if projection_method == "rounding":
-            idx_1 = torch.where(self.A_tilde >= 0.5)
-            A_proj = torch.zeros_like(self.A_tilde)
+            idx_1 = torch.where(A_tilde >= 0.5)
+            A_proj = torch.zeros_like(A_tilde)
             A_proj[idx_1] = 1.0
             return A_proj
         elif projection_method == "random":
-            A_proj = torch.randint(0, 2, self.A_tilde.shape).double()
+            A_proj = torch.randint(0, 2, A_tilde.shape).double()
             A_proj = torch.triu(A_proj) + torch.triu(A_proj, 1).T
             A_proj.fill_diagonal_(0.0)
             return A_proj
         elif projection_method == "copy":
-            return self.A_tilde.clone()
+            return A_tilde.clone()
     
-    def clip_adjacency_matrix(self, sigma_i):
+    def clip_adjacency_matrix(self, A_tilde, sigma_i):
         min_clip, max_clip = 0.0 - sigma_i, 1.0 + sigma_i
-        return torch.clip(self.A_tilde, min_clip, max_clip)
-    
-    def update_theta_annealed_prior(self, sigma_i_sq):
-        new_scale = torch.sqrt(self.theta_prior_var + sigma_i_sq)
-        self.theta_prior_dist = torch.distributions.Normal(self.theta_prior_mean, new_scale)
+        return torch.clip(A_tilde, min_clip, max_clip)
+
 
 class AdamEstimator:
     def __init__(self, h_theta, sigma_e, lr, n_iter):
--- langevin/plot_results.py
+++ langevin/plot_results.py
@@ -4,17 +4,32 @@ import pandas as pd
 from sklearn.metrics import roc_auc_score
 
 
-def plot_results(filename, legend):
+def plot_graph_results(filename, legend):
     methods = list(legend.keys())
     results = pd.read_csv(filename, index_col=0, sep=";")
     print("Samples used:", len(results) // (results.num_obs.nunique()))
-    for col in ["real_values"] + [f"pred_{method}" for method in methods]:
+    for col in ["real_graph"] + [f"graph_{method}" for method in methods]:
         results[col] = results[col].apply(lambda x: np.array(eval(x)))
     for method in methods:
-        results[legend[method]] = results.apply(lambda row: roc_auc_score(row["real_values"], row[f"pred_{method}"]), axis=1)
-    results.groupby("num_obs")[[col for col in results.columns if col in legend.values()]].mean().plot(marker="o")
+        results[legend[method]] = results.apply(lambda row: roc_auc_score(row["real_graph"], row[f"graph_{method}"]), axis=1)
+    results.groupby("num_obs")[[col for col in results.columns if col in legend.values()]].median().plot(marker="o")
     plt.xlabel("Number of observations")
     plt.ylabel("AUCROC")
+    plt.xticks(results.num_obs.unique())
+    plt.grid()
+    plt.show()
+
+def plot_theta_results(filename, legend):
+    methods = list(legend.keys())
+    results = pd.read_csv(filename, index_col=0, sep=";")
+    print("Samples used:", len(results) // (results.num_obs.nunique()))
+    for col in ["real_theta"] + [f"theta_{method}" for method in methods]:
+        results[col] = results[col].astype(float)
+    for method in methods:
+        results[legend[method]] = results.apply(lambda row: np.abs(row["real_theta"] - row[f"theta_{method}"]) / row["real_theta"], axis=1)
+    results.groupby("num_obs")[[col for col in results.columns if col in legend.values()]].median().plot(marker="o")
+    plt.xlabel("Number of observations")
+    plt.ylabel("Relative error")
     plt.xticks(results.num_obs.unique())
     plt.grid()
     plt.show()
